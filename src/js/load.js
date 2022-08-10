7a6954e2e1264a344334fdee4b2ac18c3bc0e810
Mohammed Banani | MbDev <leedbn1@gmail.com>
2022-08-10 22:15:03 +0100

    ok

---
+++ src/js/load.js
@@ -0,0 +1,4 @@
+$(function () {
+  $('#navbar').load('navbar.html');
+  $('#intro').load('intro.html');
+});
---
+++ src/js/navControl.js
@@ -0,0 +1,16 @@
+$(function () {
+  let navElems = $('#navelems')[0];
+  let button = $('#nav-toggle');
+  let btn = $('#nav-toggle')[0];
+  const nav = document.getElementById('navelems');
+  console.log(button);
+
+  button.on('click', function () {
+    btn.classList.toggle('open');
+    navElems.classList.toggle('hidden');
+    navElems.classList.toggle('flex');
+    button.hasClass('open')
+      ? $('#nav-toggle img:first-child').attr('src', './assets/close.png')
+      : $('#nav-toggle img:first-child').attr('src', './assets/more.png');
+  });
+});
